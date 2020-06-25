0b21e12672862ca2006991b4d1d50c9fd82b690e
Craig Fowler <craig@howler.la>
2020-06-25 15:26:55 -0700

    init

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,8 @@
+<header class="banner h-screen text-white bg-black flex items-center justify-center">
+  <h1>
+    <span class="sr-only">{{ $siteName }}</span>
+    <a class="brand" href="{{ home_url('/') }}">
+      <img class="w-64 block" src="@asset('images/82logo.svg')">
+    </a>
+  </h1>
+</header>
