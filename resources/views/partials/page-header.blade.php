5e081f72f06ecc3c6c7b17360758c66c3bc6990c
Craig Fowler <craig@howler.la>
2020-08-13 10:43:33 -0700

    page header

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,8 +1,8 @@
 <div class="page-header mb-8 overflow-hidden">
-  <div class="rellax">
+  <div class="">
     <div class="md:h-header-sm xl:h-header-lg overflow-hidden">
       @php $image = get_field('header_image'); $size = 'large'; @endphp
-      @php echo wp_get_attachment_image( $image, $size, '', array( 'class' => 'w-full h-full object-cover object-center' ) ) @endphp
+      @php echo wp_get_attachment_image( $image, $size, '', array( 'class' => 'w-full h-full object-cover object-center rellax' ) ) @endphp
     </div>
   </div>
   <h1 class="sr-only">{!! $title !!}</h1>
