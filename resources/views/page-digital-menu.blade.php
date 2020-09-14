4315d641afd063d60a654507ac82ea2693aecd11
Craig Fowler <craig@howler.la>
2020-09-14 11:04:04 -0700

    things

--- resources/views/page-digital-menu.blade.php
+++ resources/views/page-digital-menu.blade.php
@@ -11,9 +11,11 @@
 @endphp
 
 <div class="h-screen relative bg-black">
+  @if($image_two)
   <div class="absolute inset-0 z-20">
     <img class="h-full w-full object-contain" src="@php echo $image_two @endphp">
   </div>
+  @endif
   <div class="absolute inset-0">
     @php echo wp_get_attachment_image( $image_one, 'large', '', array( 'class' => 'h-full w-full object-contain' ) ) @endphp
   </div>
