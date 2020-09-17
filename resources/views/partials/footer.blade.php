cf2182b3512748897fcfb2ba9001f141d72495f8
Craig Fowler <craig@howler.la>
2020-09-17 13:59:45 -0700

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,7 @@
+@if(is_front_page()) <div class="fixed lg:relative bottom-0 w-full"> @endif
 <footer class="h-16 flex items-center">
   <div class="container text-sm sm:text-base">
     @php(dynamic_sidebar('sidebar-footer'))
   </div>
 </footer>
+@if(is_front_page()) </div> @endif
