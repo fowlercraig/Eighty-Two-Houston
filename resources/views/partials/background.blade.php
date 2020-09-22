73a579492249b868da24d93e93c636fc7f075320
Craig Fowler <craig@howler.la>
2020-09-22 16:51:06 -0700

    junk

--- resources/views/partials/background.blade.php
+++ resources/views/partials/background.blade.php
@@ -1,3 +1,4 @@
 <div class="fixed inset-0" style="z-index: -1">
-  <img src="@asset('images/82_Desktop_website_v005.jpg')" class="object-cover w-full h-full opacity-75">
+  <img src="@asset('images/82_Desktop_website_v005.jpg')" class="object-cover w-full h-full opacity-75 hidden lg:block">
+  <img src="@asset('images/82_website_v004_MOBILE.jpg')" class="object-cover w-full h-full opacity-75 block  lg:hidden">
 </div>
