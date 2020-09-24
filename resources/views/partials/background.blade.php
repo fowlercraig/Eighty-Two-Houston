b2997b7e1baeb70d62d8fbb350ae9563ed30cbfd
Craig Fowler <craig@howler.la>
2020-09-24 10:12:53 -0700

    background

--- resources/views/partials/background.blade.php
+++ resources/views/partials/background.blade.php
@@ -1,4 +1,8 @@
-<div class="absolute md:fixed inset-0" style="z-index: -1">
+<div class="fixed inset-0" style="z-index: -1">
+  <div class="aspect-ratio aspect-ratio--9x16 block sm:hidden">
+    <div class="absolute inset-0">
+      <img src="@asset('images/82_website_v004_MOBILE.jpg')" class="object-cover w-full h-full opacity-75">
+    </div>
+  </div>
   <img src="@asset('images/82_Desktop_website_v005.jpg')" class="object-cover w-full h-full opacity-75 hidden sm:block">
-  <img src="@asset('images/82_website_v004_MOBILE.jpg')" class="object-cover w-full h-full opacity-75 block sm:hidden">
 </div>
